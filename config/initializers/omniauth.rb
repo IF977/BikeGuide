812c5597bde4d687995f448a9bdae1faeb5fda09
Thiago Guimaraes <tgo@cin.ufpe.br>
2017-05-29 22:56:11 +0000

    Commit geral

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['CLIENT_ID'], ENV['2UYd_GJ6TotUonk7hnl_FXvY'], {
+  provider :google_oauth2, ENV['404426272416-rgrlrkgs7b5d33cr2pfgutdj79cn2je8.apps.googleusercontent.com'], ENV['2UYd_GJ6TotUonk7hnl_FXvY'], {
   scope: 'profile', image_aspect_ratio: 'square', image_size: 48, access_type: 'online'}
 end
