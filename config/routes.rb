f48ee95404e6988e0efcf49e16bc40d27c3ffdb9
Thiago Guimaraes <tgo@cin.ufpe.br>
2017-06-05 18:33:19 +0000

    User dev

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'user_sessions/new'
+
+  get 'user_sessions/create'
+
+  get 'user_sessions/destroy'
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
   get 'pages/contato' => 'pages#contato'
