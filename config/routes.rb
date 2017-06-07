b9eb0ed82966842815b3531c31cb460d523a8143
Thiago Guimaraes <tgo@cin.ufpe.br>
2017-06-07 21:36:19 +0000

    Delete login dev, improve some routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,5 @@ Rails.application.routes.draw do
   get 'pages/index' => 'pages#index'
   get 'pages/contato' => 'pages#contato'
   get 'pages/sobrenos' => 'pages#sobrenos'
-  get 'users/new' => 'users#new'
-  resources :user_sessions
-  resources :users
- 
-
-  get 'user_sessions/create'
-  get 'user_sessions/destroy'
-  get 'user_sessions/new' => 'user_sessions#new'
-  get 'user_sessions/destroy' => 'user_sessions#destroy'
  
 end
