a912bbfe86eff3b0d8debea34fe43b4ff206f110
Thiago Guimaraes <tgo@cin.ufpe.br>
2017-06-08 02:04:43 +0000

    Login Dev

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
   def new
   end
+  
   def create
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
-        session[:user_id] = user.id
+    users = Users.find_by_email(params[:email])
+    if users && users.authenticate(params[:password])
+        session[:users_id] = users.id
         redirect_to root_url, notice: "Logged in!"
         else
         flash.now.alert = "Email or password is invalid"
@@ -13,7 +14,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil
+    session[:users_id] = nil
     redirect_to root_url, notice: "Logged out!"
 
   end
